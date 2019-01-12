3f61dd4fbe0f4e0755ee3a63cafdb1358998626d
dancofer96 <dancofer96@gmail.com>
2019-01-12 14:05:17 -0500

    Refactoring record calculation into Record class plus adding season

--- src/main/java/com/cofer/projects/retrosheet/core/Record.java
+++ src/main/java/com/cofer/projects/retrosheet/core/Record.java
@@ -1,20 +1,36 @@
 package com.cofer.projects.retrosheet.core;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import java.util.Objects;
+
 public class Record {
+    @JsonProperty("team")
     private String team;
 
+    @JsonProperty("season")
+    private String season;
+
+    @JsonProperty("wins")
     private int wins;
 
+    @JsonProperty("losses")
     private int losses;
 
-    public Record(String team) {
+    public Record() {
+    }
+
+    public Record(String team, String season) {
         this.team = team;
+        this.season = season;
         this.wins = 0;
         this.losses = 0;
     }
 
-    public Record(String team, int wins, int losses) {
+    public Record(String team, String season, int wins, int losses) {
         this.team = team;
+        this.season = season;
         this.wins = wins;
         this.losses = losses;
     }
@@ -43,11 +59,47 @@ public class Record {
         this.losses = losses;
     }
 
-    public void addWin() {
+    private void addWin() {
         wins++;
     }
 
-    public void addLoss() {
+    private void addLoss() {
         losses++;
+    }
+
+    public static Record calculateRecordForTeam(List<Game> games, String team, String season) {
+        Record record = new Record(team, season);
+        games.forEach(
+                game -> {
+                    if (game.getSeason().equals(season) && team.equals(game.getWinningTeam())) {
+                        record.addWin();
+                    } else if (game.getSeason().equals(season) && team.equals(game.getLosingTeam())) {
+                        record.addLoss();
+                    }
+                }
+        );
+        return record;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Record)) {
+            return false;
+        }
+
+        final Record that = (Record) o;
+
+        return Objects.equals(this.team, that.team) &&
+                Objects.equals(this.season, that.season) &&
+                Objects.equals(this.wins, that.wins) &&
+                Objects.equals(this.losses, that.losses);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(team, season, wins, losses);
     }
 }
